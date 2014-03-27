2d6f890b58f04e5923b675e2e859b1e8fa5f0c76
Emil Nilsson <emil_nilsson_96@live.se>
2014-03-27 14:52:03 +0100

    Create config.ini.php

---
+++ config.ini.php
@@ -0,0 +1,18 @@
+<?php return ?>;
+
+[DATABASE_CONNECTION]
+host = "localhost"
+user = "root"
+pass = "p12331"
+dbname = "test22"
+
+
+; DO NOT CHANGE THE CODE BELOW
+[DATABASE_DRIVERS]
+driver[1] = "mysql"
+driver[2] = "cubrid"
+driver[3] = "mssql"
+driver[4] = "firebird"
+driver[5] = "odbc"
+driver[6] = "pgsql"
+driver[7] = "sqlite"
