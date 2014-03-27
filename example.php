9cb235e0c64c50e1ff2f69793392e3825d8bd5ac
Emil Nilsson <emil_nilsson_96@live.se>
2014-03-27 14:57:35 +0100

    Update example.php

--- example.php
+++ example.php
@@ -3,3 +3,12 @@ require_once("path/class.pdo_crud.php");
 $db = new PDO_CRUD("config.ini.php");
 
 
+/*
+* #SELECT QUERY
+*/
+  $select = $db->query("SELECT * FROM users");
+
+  foreach($select as $user_info)
+  {
+    echo $user_info['username'];
+  }
